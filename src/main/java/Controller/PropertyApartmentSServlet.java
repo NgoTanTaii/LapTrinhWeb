eded1457ea71589b8b62798cc5b08ee803d975b2
NgoTanTaii <22130239@st.hcmuaf.edu.vn>
2025-01-09 08:55:31 +0700

    tao class apartmentSServlet va them mot so chuc nang hien thi

---
+++ src/main/java/Controller/PropertyApartmentSServlet.java
@@ -0,0 +1,34 @@
+package Controller;
+import Dao.PropertyDAO;
+import Entity.Property;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+@WebServlet("/apartmentS")
+public class PropertyApartmentSServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Property> propertyList ;
+        PropertyDAO dao = new PropertyDAO();
+        try {
+            propertyList = dao.getListPropertiesType("apartmentS");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        // Truyền dữ liệu vào request để hiển thị trong JSP
+        request.setAttribute("properties", propertyList);
+
+
+        // Chuyển tiếp tới trang home.jsp để hiển thị
+        request.getRequestDispatcher("apartment-sale.jsp").forward(request, response);
+
+    }}
+
+
+
